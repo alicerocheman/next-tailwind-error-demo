1bd29e10debdecd5ea9ef349be86916d41db8ff1
Alice Rocheman <arocheman@ruedesvignerons.com>
2021-05-20 15:33:34 +0200

    hello

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className="sticky top-0 w-full bg-red-500">Hello</div>
+        <div className="sticky top-0 w-full bg-red-500">Hello, I'm a sticky</div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
