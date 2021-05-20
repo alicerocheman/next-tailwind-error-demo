5a8559641fe6e2e0d586431e1fbfb9d99612a69d
Alice Rocheman <arocheman@ruedesvignerons.com>
2021-05-20 15:53:10 +0200

    add other postcss config attempts in comments

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,21 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  }
-}
+  },
+};
+
+// // I also tried
+// module.exports = {
+//   plugins: ['tailwindcss', 'autoprefixer'],
+// };
+
+// // and
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'),
+//     require('autoprefixer'),
+//   ]
+// };
+// // as indicated in tailwind css documentation
+// // but produces an error:
+// // Error: A PostCSS Plugin was passed as a function using require(), but it must be provided as a string.
